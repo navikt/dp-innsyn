641e9689d285dad7bbf9f984fe1ff23611add87f
André Roaldseth <andre.roaldseth@nav.no>
2021-03-26 09:19:26 +0100

    Legg til feilende test for lagring

--- mediator/src/test/kotlin/no/nav/dagpenger/innsyn/db/PostgresPersonRepositoryTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/innsyn/db/PostgresPersonRepositoryTest.kt
@@ -1,9 +1,9 @@
 package no.nav.dagpenger.innsyn.db
 
-import no.nav.dagpenger.innsyn.Dagpenger.vedtakOppgave
 import no.nav.dagpenger.innsyn.helpers.Postgres.withMigratedDb
+import no.nav.dagpenger.innsyn.modell.hendelser.Oppgave.OppgaveType
 import no.nav.dagpenger.innsyn.modell.hendelser.Søknad
-import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class PostgresPersonRepositoryTest {
@@ -13,12 +13,13 @@ internal class PostgresPersonRepositoryTest {
     fun `skal lagre og finne person`() {
         withMigratedDb {
             val person = repository.person("123")
+            val testOppgave = OppgaveType("test")
 
-            person.håndter(Søknad("id", setOf()))
+            person.håndter(Søknad("id", setOf(testOppgave.ny("oppgaver"))))
             repository.lagre(person)
 
             repository.person(person.fnr).also {
-                assertFalse(it.harUferdigeOppgaverAv(vedtakOppgave))
+                assertTrue(it.harUferdigeOppgaverAv(testOppgave))
             }
         }
     }
