74b79aab0896767127f1e847ff5851111476a7e3
Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
2021-04-15 09:27:05 +0200

    Status endrer seg på hendelse når alle delhendelsene er fullført
    navikt/dagpenger#754
    
    Co-authored-by: Celine Dam <celine.ten.dam@nav.no>

--- modell/src/test/kotlin/no/nav/dagpenger/innsyn/modell/HendelseTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/innsyn/modell/HendelseTest.kt
@@ -3,6 +3,7 @@ package no.nav.dagpenger.innsyn.modell
 import no.nav.dagpenger.innsyn.modell.HendelseA.Status.FERDIG
 import no.nav.dagpenger.innsyn.modell.HendelseA.Status.PLANLAGT
 import no.nav.dagpenger.innsyn.modell.HendelseA.Status.PÅBEGYNT
+import no.nav.dagpenger.innsyn.modell.hendelser.Hendelse
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -19,44 +20,75 @@ class HendelseTest {
 
     @Test
     fun `Vi mangler 2 av 4 vedlegg for å kunne behandle søknaden din`() {
-        val hendelse = HendelseA(
-            suspensjon = LocalDateTime.now().plusDays(14),
-            components = listOf(
+        val dokumentasjon = HendelseA(
+            frist = LocalDateTime.now().plusDays(14),
+            delHendelser = listOf(
                 HendelseA().also { it.fullfør() },
                 HendelseA().also { it.fullfør() },
                 HendelseA(),
                 HendelseA()
             )
         )
-        assertEquals(hendelse.status(), PÅBEGYNT)
-        assertTrue(LocalDateTime.now().isBefore(hendelse.suspensjon))
-        assertEquals(hendelse.components.size, 4)
-        assertEquals(hendelse.components.filter { it.status() == FERDIG }.size, 2)
-        assertEquals(hendelse.components.filter { it.status() == PLANLAGT }.size, 2)
+        assertEquals(PÅBEGYNT, dokumentasjon.status())
+        assertTrue(LocalDateTime.now().isBefore(dokumentasjon.frist))
+        assertEquals(4, dokumentasjon.delHendelser.size)
+        assertEquals(2, dokumentasjon.delHendelser.filter { it.status() == FERDIG }.size)
+        assertEquals(2, dokumentasjon.delHendelser.filter { it.status() == PLANLAGT }.size)
+    }
+
+    @Test
+    fun `Når man sender inn de to siste vedleggene blir hele dokumentasjon-hendelsen FERDIG`() {
+        val vedleggA = HendelseA().also { it.fullfør() }
+        val vedleggB = HendelseA().also { it.fullfør() }
+        val vedleggC = HendelseA()
+        val vedleggD = HendelseA()
+
+        val dokumentasjon = HendelseA(
+            frist = LocalDateTime.now().plusDays(14),
+            delHendelser = listOf(
+                vedleggA,
+                vedleggB,
+                vedleggC,
+                vedleggD
+            )
+        )
+        assertEquals(PÅBEGYNT, dokumentasjon.status())
+        assertTrue(LocalDateTime.now().isBefore(dokumentasjon.frist))
+        assertEquals(dokumentasjon.delHendelser.size, 4)
+        assertEquals(dokumentasjon.delHendelser.filter { it.status() == FERDIG }.size, 2)
+        assertEquals(dokumentasjon.delHendelser.filter { it.status() == PLANLAGT }.size, 2)
+
+        vedleggC.fullfør()
+        vedleggD.fullfør()
+
+        assertEquals(4, dokumentasjon.delHendelser.filter { it.status() == FERDIG }.size)
+        assertEquals(0, dokumentasjon.delHendelser.filter { it.status() == PLANLAGT }.size)
+        assertEquals(FERDIG, dokumentasjon.status())
     }
 
     @Test
     fun `Når saken er ferdig behandlet vil du få varsel på SMS`() {
         val hendelse = HendelseA(
-            suspensjon = LocalDateTime.now().plusDays(14)
+            frist = LocalDateTime.now().plusDays(14)
         )
         assertEquals(hendelse.status(), PLANLAGT)
-        assertTrue(LocalDateTime.now().isBefore(hendelse.suspensjon))
+        assertTrue(LocalDateTime.now().isBefore(hendelse.frist))
     }
 }
 
 class HendelseA(
     var fullført: LocalDateTime? = null,
     var status: Status = PLANLAGT,
-    val suspensjon: LocalDateTime? = null,
-    val components: List<HendelseA> = emptyList()
+    val frist: LocalDateTime? = null,
+    val delHendelser: List<HendelseA> = emptyList()
 ) {
     fun status(): Status {
-        if (components.isNotEmpty()) {
-            return if (components.any { it.status() == FERDIG })
-                PÅBEGYNT
-            else
-                PLANLAGT
+        if (delHendelser.isNotEmpty()) {
+            return when {
+                delHendelser.all { it.status() == FERDIG } -> FERDIG
+                delHendelser.any { it.status() == FERDIG } -> PÅBEGYNT
+                else -> PLANLAGT
+            }
         }
         return status
     }
