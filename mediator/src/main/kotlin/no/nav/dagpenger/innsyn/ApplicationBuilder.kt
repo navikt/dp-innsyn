de7d866a9a425303d8a0acf803fe094b0e8e0fac
André Roaldseth <andre.roaldseth@nav.no>
2021-05-14 11:59:39 +0200

    Skru av tømming av databasen ved hver deploy

--- mediator/src/main/kotlin/no/nav/dagpenger/innsyn/ApplicationBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/innsyn/ApplicationBuilder.kt
@@ -1,6 +1,5 @@
 package no.nav.dagpenger.innsyn
 
-import no.nav.dagpenger.innsyn.db.PostgresDataSourceBuilder.clean
 import no.nav.dagpenger.innsyn.db.PostgresDataSourceBuilder.runMigration
 import no.nav.dagpenger.innsyn.db.PostgresPersonRepository
 import no.nav.dagpenger.innsyn.tjenester.EttersendingMottak
@@ -33,7 +32,6 @@ internal class ApplicationBuilder(env: Map<String, String>) : RapidsConnection.S
     fun start() = rapidsConnection.start()
 
     override fun onStartup(rapidsConnection: RapidsConnection) {
-        clean()
         runMigration()
     }
 }
