1788828019ccbbcd09d816d6f9402f667932f1c2
André Roaldseth <andre.roaldseth@nav.no>
2021-05-14 12:42:39 +0200

    Logg litt mer strukturert tolkning av oppgaver

--- mediator/src/main/kotlin/no/nav/dagpenger/innsyn/tjenester/OppgaveMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/innsyn/tjenester/OppgaveMottak.kt
@@ -1,12 +1,15 @@
 package no.nav.dagpenger.innsyn.tjenester
 
 import mu.KotlinLogging
+import mu.withLoggingContext
 import no.nav.dagpenger.innsyn.PersonMediator
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 private val logg = KotlinLogging.logger {}
 private val sikkerlogg = KotlinLogging.logger("tjenestekall.OppgaveMottak")
@@ -18,13 +21,43 @@ internal class OppgaveMottak(
     init {
         River(rapidsConnection).apply {
             validate { it.demandValue("table", "SIAMO.OPPGAVE_LOGG") }
+            validate { it.demandValue("after.SAK_TYPE", "DAGP") }
+            validate {
+                it.requireKey(
+                    "after.SAK_ID",
+                    "after.USERNAME",
+                    "after.DUEDATE",
+                    "after.KONTOR",
+                    "after.OPPGAVETYPE_BESKRIVELSE",
+                    "after.OPERASJON",
+                    "after.TRANS_ID",
+                )
+            }
             validate { it.requireKey("after") }
         }.register(this)
     }
 
+    companion object {
+        private var formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    }
+
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        logg.info { "Mottok oppgavelogg" }
-        sikkerlogg.info { "Mottok oppgave: ${packet.toJson()}" }
+        val sakId = packet["after"]["SAK_ID"].asText()
+        val transaksjonId = packet["after"]["TRANS_ID"].asText()
+        val benk = packet["after"]["KONTOR"].asText()
+
+        withLoggingContext(
+            "sakId" to sakId,
+            "benk" to benk,
+            "transaksjonId" to transaksjonId,
+        ) {
+            val operasjon = packet["after"]["OPERASJON"].asText()
+            val navn = packet["after"]["OPPGAVETYPE_BESKRIVELSE"].asText()
+            val frist = packet["after"]["DUEDATE"].asText().let { LocalDateTime.parse(it, formatter) }
+
+            logg.info { "Mottok operasjon $operasjon på oppgave $navn. Ligger på benk $benk, med frist til $frist." }
+            // sikkerlogg.info { "Mottok oppgave: ${packet.toJson()}" }
+        }
     }
 
     override fun onError(problems: MessageProblems, context: MessageContext) {
