691853aec9557bcec60d3202822eea8a07f4a41c
Giao The Cung <giaothe@gmail.com>
2022-06-03 15:53:15 +0200

    Oppgradere til ktor 2.0.2

--- mediator/src/main/kotlin/no/nav/dagpenger/innsyn/tjenester/HenvendelseOppslag.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/innsyn/tjenester/HenvendelseOppslag.kt
@@ -3,22 +3,23 @@ package no.nav.dagpenger.innsyn.tjenester
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonMapperBuilder
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.cio.CIO
-import io.ktor.client.features.DefaultRequest
-import io.ktor.client.features.json.JacksonSerializer
-import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.header
 import io.ktor.client.request.request
+import io.ktor.client.request.setBody
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
+import io.ktor.serialization.jackson.jackson
 import no.nav.dagpenger.innsyn.tjenester.ettersending.MinimalEttersendingDto
 import no.nav.dagpenger.innsyn.tjenester.ettersending.toInternal
 import no.nav.dagpenger.innsyn.tjenester.paabegynt.Påbegynt
 import no.nav.dagpenger.innsyn.tjenester.paabegynt.toInternal
-import no.nav.dagpenger.ktor.client.metrics.PrometheusMetrics
+import no.nav.dagpenger.ktor.client.metrics.PrometheusMetricsPlugin
 import java.time.ZonedDateTime
 
 internal class HenvendelseOppslag(
@@ -30,20 +31,18 @@ internal class HenvendelseOppslag(
 
     private val dpProxyClient = HttpClient(httpClientEngine) {
 
-        install(PrometheusMetrics) {
+        install(PrometheusMetricsPlugin) {
             this.baseName = baseName
         }
 
         install(DefaultRequest) {
         }
-        install(JsonFeature) {
-            serializer = JacksonSerializer(
-                jackson = jacksonMapperBuilder()
-                    .addModule(JavaTimeModule())
-                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                    .build()
-            )
+        install(ContentNegotiation) {
+            jackson {
+                registerModule(JavaTimeModule())
+                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            }
         }
     }
 
@@ -61,8 +60,8 @@ internal class HenvendelseOppslag(
             header(HttpHeaders.Authorization, "Bearer ${tokenProvider.invoke()}")
             header(HttpHeaders.ContentType, "application/json")
             header(HttpHeaders.Accept, "application/json")
-            body = mapOf("fnr" to fnr)
-        }
+            setBody(mapOf("fnr" to fnr))
+        }.body()
 }
 
 data class ExternalEttersending(
