279acaa193ebf0f7da4df045344393c6aed7086e
André Roaldseth <andre.roaldseth@nav.no>
2022-06-20 21:27:11 +0200

    More fast build

--- buildSrc/src/main/kotlin/dagpenger.rapid-and-rivers.gradle.kts
+++ buildSrc/src/main/kotlin/dagpenger.rapid-and-rivers.gradle.kts
@@ -13,16 +13,3 @@ dependencies {
     implementation(Konfig.konfig)
     implementation(Kotlin.Logging.kotlinLogging)
 }
-
-tasks.withType<Jar>().configureEach {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    manifest {
-        attributes(mapOf("Main-Class" to application.mainClass.get()))
-    }
-
-    from(
-        configurations.runtimeClasspath.get().map {
-            if (it.isDirectory) it else zipTree(it)
-        }
-    )
-}
