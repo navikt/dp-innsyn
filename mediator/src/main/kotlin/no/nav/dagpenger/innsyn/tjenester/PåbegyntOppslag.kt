e263d7d2af07a2e9c9e1b0097fce24a60e14e8af
Marius Eriksen <marius.eriksen@nav.no>
2022-11-02 09:43:52 +0100

    F start på henting av påbegynt søknad fra ny søknadsdialog
    DAG-494
    
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>

---
+++ mediator/src/main/kotlin/no/nav/dagpenger/innsyn/tjenester/PåbegyntOppslag.kt
@@ -0,0 +1,64 @@
+package no.nav.dagpenger.innsyn.tjenester
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.contentType
+import io.ktor.serialization.jackson.jackson
+import java.time.ZonedDateTime
+import java.util.UUID
+
+internal class PåbegyntOppslag(
+    private val baseUrl: String,
+    private val soknadAudience: String = "soknadAudience",
+    private val tokenProvider: (token: String, audience: String) -> String = exchangeToOboToken,
+    engine: HttpClientEngine = CIO.create()
+) {
+
+    private val httpClient = HttpClient(engine) {
+        expectSuccess = true
+        install(ContentNegotiation) {
+            jackson {
+                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
+                registerModule(JavaTimeModule())
+            }
+        }
+    }
+
+    internal suspend fun hentPåbegyntSøknad(subjectToken: String): PåbegyntSøknadDto {
+        val url = "$baseUrl/soknad/paabegynt"
+        return httpClient.get(url) {
+            addBearerToken(subjectToken)
+            contentType(ContentType.Application.Json)
+        }.body()
+    }
+
+    private fun HttpRequestBuilder.addBearerToken(subjectToken: String) {
+        headers[HttpHeaders.Authorization] = "Bearer ${tokenProvider.invoke(subjectToken, soknadAudience)}"
+    }
+}
+
+data class PåbegyntSøknadDto(
+    @JsonProperty("uuid")
+    val uuid: UUID,
+    @JsonProperty("opprettet")
+    val opprettet: ZonedDateTime,
+    @JsonProperty("sistEndret")
+    val sistEndret: ZonedDateTime?,
+)
+
+private val exchangeToOboToken = { token: String, audience: String ->
+    "dummyToken"
+    // val accessTokenResponse = Configuration.tokenXClient.tokenExchange(token, audience)
+    // accessTokenResponse.accessToken
+}
---
+++ mediator/src/test/kotlin/no/nav/dagpenger/innsyn/tjenester/PåbegynteOppslagTest.kt
@@ -0,0 +1,67 @@
+package no.nav.dagpenger.innsyn.tjenester
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.headersOf
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.util.UUID
+
+class PåbegynteOppslagTest {
+
+    private val testTokenProvider: (token: String, audience: String) -> String = { _, _ -> "testToken" }
+    private val baseUrl = "http://baseurl"
+    private val soknadAudience = "dp-soknad"
+    private val subjectToken = "subjectToken"
+
+    @Test
+    fun `Henter påbegynt søknad`() {
+        val søknadUuid = UUID.randomUUID()
+        val opprettet = ZonedDateTime.of(LocalDateTime.MAX, ZoneId.of("Europe/Oslo"))
+        val sistEndret = ZonedDateTime.of(LocalDateTime.MAX, ZoneId.of("Europe/Oslo"))
+        runBlocking {
+            val påbegyntOppslag = PåbegyntOppslag(
+                baseUrl,
+                soknadAudience,
+                testTokenProvider,
+                engine = MockEngine { request ->
+                    assertEquals("$baseUrl/soknad/paabegynt", request.url.toString())
+                    assertEquals(HttpMethod.Get, request.method)
+                    assertEquals(
+                        "Bearer ${testTokenProvider.invoke(subjectToken, soknadAudience)}",
+                        request.headers[HttpHeaders.Authorization]
+                    )
+                    val jsonResponse = objectMapper.writeValueAsString(
+                        PåbegyntSøknadDto(
+                            uuid = søknadUuid,
+                            opprettet = opprettet,
+                            sistEndret = sistEndret
+                        )
+                    )
+                    respond(
+                        content = jsonResponse,
+                        headers = headersOf(HttpHeaders.ContentType, "application/json")
+                    )
+                }
+            )
+
+            val påbegyntResponse = påbegyntOppslag.hentPåbegyntSøknad(subjectToken = "testToken")
+            assertEquals(søknadUuid, påbegyntResponse.uuid)
+            assertEquals(opprettet.toOffsetDateTime(), påbegyntResponse.opprettet.toOffsetDateTime())
+            assertEquals(sistEndret.toOffsetDateTime(), påbegyntResponse.sistEndret!!.toOffsetDateTime())
+        }
+    }
+
+    private val objectMapper = jacksonObjectMapper()
+        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        .registerModule(JavaTimeModule())
+}
