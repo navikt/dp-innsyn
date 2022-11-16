7ee5d88cfbe28ae4e09b97c1eb52c072acda64a6
Marius Eriksen <marius.eriksen@nav.no>
2022-11-16 10:57:02 +0100

    Logg behandlingsstatus ifm illegal json parameter feilmelding i dp-soknad

--- mediator/src/main/kotlin/no/nav/dagpenger/innsyn/InnsynApi.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/innsyn/InnsynApi.kt
@@ -50,6 +50,7 @@ import java.time.LocalDate
 import java.util.UUID
 
 private val logger = KotlinLogging.logger { }
+private val sikkerlogg = KotlinLogging.logger("tjenestekall.InnsynApi")
 
 internal fun Application.innsynApi(
     jwkProvider: JwkProvider,
@@ -154,7 +155,7 @@ internal fun Application.innsynApi(
                 val fom = call.request.queryParameters["fom"]
                     ?: throw IllegalArgumentException("Mangler fom queryparameter i url")
                 val behandlingsstatus = avgjørBehandlingsstatus.hentStatus(fnr, LocalDate.parse(fom))
-
+                sikkerlogg.info { "Behandlingsstatus: $behandlingsstatus" }
                 call.respond(HttpStatusCode.OK, BehandlingsstatusDTO(behandlingsstatus))
             }
 
