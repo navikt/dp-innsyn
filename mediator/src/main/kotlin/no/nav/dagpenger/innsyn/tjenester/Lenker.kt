3449ae5f70c214a877db37ab290b5efd4f5d701b
André Roaldseth <andre.roaldseth@nav.no>
2022-12-15 09:17:45 +0100

    Endre lenker til ettersending til ny generell innsending
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>
    Co-authored-by: Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
    Co-authored-by: Ingrid Håland <Ingrid.Haland@nav.no>
    Co-authored-by: Hanne Eilertsen Olsen <hanne.eilertsen.olsen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/innsyn/tjenester/Lenker.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/innsyn/tjenester/Lenker.kt
@@ -6,5 +6,5 @@ object Lenker {
     fun påbegyntNySøknadsdialogIngress(søknadId: String) = Configuration.nySøknadsdialogIngress + "/$søknadId"
     fun påbegyntGammelSøknadsdialogIngress(søknadId: String) = Configuration.gammelSøknadsdialogIngress + "/soknad/$søknadId"
     fun ettersendelseNySøknadsdialog(søknadId: String): String = påbegyntNySøknadsdialogIngress(søknadId) + "/kvittering"
-    fun ettersendelseGammelSøknadsdialog(søknadId: String): String = Configuration.gammelSøknadsdialogIngress + "/startettersending/$søknadId"
+    fun ettersendelseGammelSøknadsdialog(søknadId: String): String = Configuration.nySøknadsdialogIngress + "/../generell-innsending"
 }
